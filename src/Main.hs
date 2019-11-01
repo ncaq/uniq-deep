b679ed1e51321da7069e90b097e08270716c9470
ncaq <ncaq@ncaq.net>
2019-11-01 10:22:35 +0900

    modified: Set -> HashSet

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import qualified Data.ByteString.Char8 as B
-import qualified Data.Set              as S
+import qualified Data.HashSet          as S
 
 main :: IO ()
 main = uniqDeep
@@ -12,7 +12,7 @@ uniqDeep = B.getContents >>= B.putStrLn . B.unlines . deleteDuplicate . B.lines
 deleteDuplicate :: [B.ByteString] -> [B.ByteString]
 deleteDuplicate = check S.empty
 
-check :: S.Set B.ByteString -> [B.ByteString] -> [B.ByteString]
+check :: S.HashSet B.ByteString -> [B.ByteString] -> [B.ByteString]
 check _ [] = []
 check already (l : ls)
   | l `S.member` already = check already ls
